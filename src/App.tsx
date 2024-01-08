e28abc93ca2fda8628359446ceef9d9c975104b8
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-01-08 22:36:49 +0530

    base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello Dukaan!</h1>;
+  return (
+    <div className="container">
+      <div className="sideNav">
+        <div className="navTop">
+          <div className="storeProfile"></div>
+          <div className="navMenu"></div>
+        </div>
+        <div className="navBottom"></div>
+      </div>
+      <div className="mainView">
+        <div className="dashboardHeader"></div>
+        <div className="dashboard">
+          <div className="dashboardDetails"></div>
+          <div className="dashboardTable"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
